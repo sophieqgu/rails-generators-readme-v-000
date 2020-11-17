ffef26ecf2415176c23379326b3fe616ba4b896e
Sophie Gu <sophieqgu@gmail.com>
2020-11-17 03:16:45 +0000

    Automatically backed up by Learn

--- db/migrate/20201117031535_add_post_status_to_posts.rb
+++ db/migrate/20201117031535_add_post_status_to_posts.rb
@@ -1,6 +1,5 @@
 class AddPostStatusToPosts < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :post_status, :boolean
-    add_column :posts, :==no-test-framework, :string
   end
 end
